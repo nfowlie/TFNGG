1f73e52b4f51d02993a183f265db7148653cfc52
Neil <neil.fowlie@gmail.com>
2018-02-19 01:42:51 -0500

    Inventory and LootBoxes Seperated.
    Still Need to add Filters to Loot.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // window.localStorage.clear();
 var unlockedSongs = window.localStorage;
 var tracksUnlocked = unlockedSongs.length - 1;
-var lootBox = document.querySelector(".lootbox");
+var lootBox = document.querySelector(".lootbox-lock");
 
+
+// Navigation
+document.querySelector("#loot").addEventListener("click", function(){
+    document.querySelector(".lootbox").style.display = "flex";
+    document.querySelector("#tracks").style.display = "none";
+});
+
+document.querySelector("#track").addEventListener("click", function(){
+    document.querySelector(".lootbox").style.display = "none";
+    document.querySelector("#tracks").style.display = "flex";
+});
 
 // Handles Lootbox 'Animation'
 // Mouse Enter
@@ -16,13 +27,16 @@ lootBox.addEventListener("mouseout", function () {
 // Mouse Click Release
 lootBox.addEventListener("mouseup", function () {
     var masterPlayer = document.querySelector(".player audio");
-    var trackSrc = "./tracks/" + tracks[getRandomIntInclusive(0, tracks.length - 1)].source;
+    var track = tracks[getRandomIntInclusive(0, tracks.length - 1)];
+    var trackTitle = track.title;
+    var trackSrc = "./tracks/" + track.source;
+    
     lootBox.style.backgroundImage = 'url("./loot-box-edited-pressed.png")';
     getRandomIntInclusive(0, tracks.length);
     masterPlayer.src = trackSrc;
     masterPlayer.play();
     tracksUnlocked++;
-    unlockedSongs.setItem(tracksUnlocked, trackSrc);
+    unlockedSongs.setItem(tracksUnlocked, JSON.stringify({title: trackTitle, src: trackSrc}));
     loadTracks(unlockedSongs);
 });
 // Mouse Click Down
@@ -41,15 +55,17 @@ function getRandomIntInclusive(min, max) {
 var loadTracks = function (unlockedSongs) {
     var tracksContainer = document.querySelector("#tracks");
     tracksContainer.innerHTML = "";
+
     for (var t = 0; t < unlockedSongs.length; t++) {
+        var parsed = unlockedSongs.getItem(t);
         var track = document.createElement("div");
         track.classList.add('track');
         var trackTitle = document.createElement("div");
-        trackTitle.textContent = unlockedSongs[t];
+        trackTitle.textContent = JSON.parse(parsed).title;
         track.appendChild(trackTitle);
         var trackPlayer = document.createElement("audio");
         trackPlayer.setAttribute("controls", "");
-        trackPlayer.src = unlockedSongs[t];
+        trackPlayer.src = JSON.parse(parsed).src;
         track.appendChild(trackPlayer);
         
         tracksContainer.appendChild(track);
