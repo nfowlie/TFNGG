a7cdf060623a27d1911ef7b910350c551eb7d4b2
Neil <neil.fowlie@gmail.com>
2018-02-18 19:07:02 -0500

    Unlocks playable Tracks
    Needs to check for dupes(?) and be on a seperate page.

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
-document.querySelector(".lootbox").addEventListener("mouseover", function () {
-    document.querySelector(".lootbox").style.backgroundImage = 'url("./loot-box-edited-pressed.png")';
-});
+// window.localStorage.clear();
+var unlockedSongs = window.localStorage;
+var tracksUnlocked = unlockedSongs.length - 1;
+var lootBox = document.querySelector(".lootbox");
 
-document.querySelector(".lootbox").addEventListener("mouseout", function () {
-    document.querySelector(".lootbox").style.backgroundImage = 'url("./loot-box-edited.png")';
-});
 
-document.querySelector(".lootbox").addEventListener("mouseup", function () {
-    document.querySelector(".lootbox").style.backgroundImage = 'url("./loot-box-edited-pressed.png")';
+// Handles Lootbox 'Animation'
+// Mouse Enter
+lootBox.addEventListener("mouseover", function () {
+    lootBox.style.backgroundImage = 'url("./loot-box-edited-pressed.png")';
+});
+// Mouse Leave
+lootBox.addEventListener("mouseout", function () {
+    lootBox.style.backgroundImage = 'url("./loot-box-edited.png")';
+});
+// Mouse Click Release
+lootBox.addEventListener("mouseup", function () {
+    var masterPlayer = document.querySelector(".player audio");
+    var trackSrc = "./tracks/" + tracks[getRandomIntInclusive(0, tracks.length - 1)].source;
+    lootBox.style.backgroundImage = 'url("./loot-box-edited-pressed.png")';
     getRandomIntInclusive(0, tracks.length);
-    document.querySelector("audio").src = "./tracks/" + tracks[getRandomIntInclusive(0, tracks.length - 1)].source;
-    document.querySelector("audio").play();
+    masterPlayer.src = trackSrc;
+    masterPlayer.play();
+    tracksUnlocked++;
+    unlockedSongs.setItem(tracksUnlocked, trackSrc);
+    loadTracks(unlockedSongs);
+});
+// Mouse Click Down
+lootBox.addEventListener("mousedown", function () {
+    lootBox.style.backgroundImage = 'url("./loot-box-edited-clicked.png")';
 });
 
-document.querySelector(".lootbox").addEventListener("mousedown", function () {
-    document.querySelector(".lootbox").style.backgroundImage = 'url("./loot-box-edited-clicked.png")';
-});
-
-
+// Random Number Generator
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-  }
+}
+
+// Load Tracks
+var loadTracks = function (unlockedSongs) {
+    var tracksContainer = document.querySelector("#tracks");
+    tracksContainer.innerHTML = "";
+    for (var t = 0; t < unlockedSongs.length; t++) {
+        var track = document.createElement("div");
+        track.classList.add('track');
+        var trackTitle = document.createElement("div");
+        trackTitle.textContent = unlockedSongs[t];
+        track.appendChild(trackTitle);
+        var trackPlayer = document.createElement("audio");
+        trackPlayer.setAttribute("controls", "");
+        trackPlayer.src = unlockedSongs[t];
+        track.appendChild(trackPlayer);
+        
+        tracksContainer.appendChild(track);
+    }
+};
+
+loadTracks(unlockedSongs);
